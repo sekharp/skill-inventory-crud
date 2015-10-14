8eceb87a094a18692b917f6d4ff815c5db644915
Sekhar Paladugu <sekharp@gmail.com>
2015-10-13 20:59:17 -0600

    frustrating error about join and skill

--- app/controllers/skill_inventory_app.rb
+++ app/controllers/skill_inventory_app.rb
@@ -1,7 +1,15 @@
+require 'models/skill_inventory'
+
 class SkillInventoryApp < Sinatra::Base
   set :root, File.join(File.dirname(__FILE__), '..')
+  set :method_override, true
+
+  get '/' do
+    erb :dashboard
+  end
 
   get '/skills' do
+    @skills = SkillInventory.all
     erb :index
   end
 
--- app/models/skill_inventory.rb
+++ app/models/skill_inventory.rb
@@ -1,5 +1,13 @@
+require 'yaml/store'
+require_relative 'skill'
+
 class SkillInventory
   def self.database
     @database ||= YAML::Store.new("db/skill_inventory")
   end
+
+  def self.all
+    raw_skills.map { |data| Skill.new(data) }
+  end
+
 end
